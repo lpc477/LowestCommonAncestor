f11cda814e4d94e99695ea70b5987df4d89523c8
Unknown <lpc477@gmail.com>
2019-09-21 12:42:54 +0100

    Added testFindNode()
    Added JUnit test for find node method. runs on 4 correct and ids 1 incorrect node

--- src/LCATest.java
+++ src/LCATest.java
@@ -34,6 +34,25 @@ class LCATest {
 		assertEquals('b',lca.returnKey(lca.head.left.right),"b not Created");
 		lca.addNode('e', 4);
 		assertEquals('e',lca.returnKey(lca.head.right.right),"e not Created");
-		
+	}
+	
+	//Test the find node method
+	@Test
+	public void testFindNode() {
+		LowestCommonAncestor lca = new LowestCommonAncestor();
+		lca.createHead('c', 0);
+		assertEquals('c',lca.returnKey(lca.head),"Head not Created");
+		lca.addNode('a', 1);
+		assertEquals('a',lca.returnKey(lca.head.left),"a not Created");
+		lca.addNode('d', 2);
+		assertEquals('d',lca.returnKey(lca.head.right),"d not Created");
+		lca.addNode('b', 3);
+		assertEquals('b',lca.returnKey(lca.head.left.right),"b not Created");
+		lca.addNode('e', 4);
+		assertEquals('e',lca.returnKey(lca.head.right.right),"e not Created");	
+		assertEquals('e',lca.returnKey(lca.findNode('e')),"e not found");
+		assertEquals('d',lca.returnKey(lca.findNode('d')),"d not found");
+		assertEquals('b',lca.returnKey(lca.findNode('b')),"b not found");
+		assertNull(lca.findNode('z'),"z found, but not in tree");
 	}
 }
