a06b1828d46711859b5ddc522df565ae39280aa9
Unknown <lpc477@gmail.com>
2019-09-21 12:21:59 +0100

    Test AddNode
    Added test case for addNode

--- src/LCATest.java
+++ src/LCATest.java
@@ -20,4 +20,20 @@ class LCATest {
 		lca.createHead('a', 0);
 		assertEquals('a',lca.returnKey(lca.head),"Head not Created");
 	}
+	//Test the LCA AddNode Function
+	@Test
+	public void testAddNode() {
+		LowestCommonAncestor lca = new LowestCommonAncestor();
+		lca.createHead('c', 0);
+		assertEquals('c',lca.returnKey(lca.head),"Head not Created");
+		lca.addNode('a', 1);
+		assertEquals('a',lca.returnKey(lca.head.left),"a not Created");
+		lca.addNode('d', 2);
+		assertEquals('d',lca.returnKey(lca.head.right),"d not Created");
+		lca.addNode('b', 3);
+		assertEquals('b',lca.returnKey(lca.head.left.right),"b not Created");
+		lca.addNode('e', 4);
+		assertEquals('e',lca.returnKey(lca.head.right.right),"e not Created");
+		
+	}
 }
